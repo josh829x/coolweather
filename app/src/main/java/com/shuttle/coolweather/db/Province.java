906a6c3e0425fbcba4d5c8de0e0a9a6bed60cc4b
josh829 <josh829x@gmail.com>
2017-12-14 13:51:25 +0800

    加入建立資料庫和table的各項配置

---
+++ app/src/main/java/com/shuttle/coolweather/db/Province.java
@@ -0,0 +1,38 @@
+package com.shuttle.coolweather.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by shuttle on 2017/12/14.
+ */
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public int getId() {
+
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
